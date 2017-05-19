80c38a503ec7b1185c9fe4e714582ba751ab9d60
Brendan Rogers <brendan@utexas.edu>
2017-05-18 21:58:49 -0500

    added witclient file. NLP is taking shape.

--- bin/run.js
+++ bin/run.js
@@ -1,7 +1,7 @@
 // tell node we want to use ES6 strict mode
 'use strict';
 
-const slackClient = require('../server/slackClient')
+const slackClient = require('../server/slackClient');
 const service = require('../server/service');
 // need http object from node.js core
 const http = require('http');
@@ -9,10 +9,13 @@ const http = require('http');
 // initiate the server by adding in 'service' object
 const server = http.createServer(service);
 
+const witToken = 'S46OXK2GLZQAAQT7VF7KRNG5GMZP5UG7';
+const witClient = require('../server/witclient')(witToken);
+
 const slackToken = 'xoxb-184836662593-8I3wUkDYuymr6hoh8LE9Nt5J';
 const slackLogLevel = 'verbose';
 
-const rtm = slackClient.init(slackToken, slackLogLevel);
+const rtm = slackClient.init(slackToken, slackLogLevel, witClient);
 rtm.start();
 
 slackClient.addAuthenticatedHandler(rtm, () => server.listen(3000) );
--- server/slackclient.js
+++ server/slackclient.js
@@ -2,19 +2,31 @@
 
 const RtmClient = require('@slack/client').RtmClient;
 const CLIENT_EVENTS = require('@slack/client').CLIENT_EVENTS;
-
+const RTM_EVENTS = require('@slack/client').RTM_EVENTS;
+// declaring rtm globally so it's available when we need it on line 25
+let rtm = null;
+let nlp = null;
 
 function handleOnAuthenticated(rtmStartData) {
 	console.log(`Logged in as ${rtmStartData.self.name} of team ${rtmStartData.team.name}, but not yet connected to a channel`);
+}
+
+function handleOnMessage(message) {
+	nlp.ask(message.text);
+	rtm.sendMessage('this is a test message', message.channel, function messageSent() {
+
+	});
 }
 
 function addAuthenticatedHandler(rtm, handler) {
 	rtm.on(CLIENT_EVENTS.RTM.AUTHENTICATED, handler);
 	return
 }
-module.exports.init = function slackClient(token, logLevel) {
-	const rtm = new RtmClient(token, {logLevel: 'logLevel'});
+module.exports.init = function slackClient(token, logLevel, nlpClient) {
+	rtm = new RtmClient(token, {logLevel: 'logLevel'});
+	nlp = nlpClient;
 	addAuthenticatedHandler(rtm, handleOnAuthenticated);
+	rtm.on(RTM_EVENTS.MESSAGE, handleOnMessage);
 	return rtm;
 }
 
