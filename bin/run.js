7a38a0a520de0bc2ecd81175bd0bad816e3cf980
Brendan Rogers <brendan@utexas.edu>
2017-05-18 19:35:20 -0500

    added slackclient.js file config. Made it so express only starts after slack client is connected.

--- bin/run.js
+++ bin/run.js
@@ -1,12 +1,21 @@
 // tell node we want to use ES6 strict mode
 'use strict';
 
+const slackClient = require('../server/slackClient')
 const service = require('../server/service');
 // need http object from node.js core
 const http = require('http');
 
 // initiate the server by adding in 'service' object
 const server = http.createServer(service);
+
+const slackToken = 'xoxb-184836662593-8I3wUkDYuymr6hoh8LE9Nt5J';
+const slackLogLevel = 'verbose';
+
+const rtm = slackClient.init(slackToken, slackLogLevel);
+rtm.start();
+
+slackClient.addAuthenticatedHandler(rtm, () => server.listen(3000) );
 
 // listen on port 3000 with our server
 server.listen(3000);
