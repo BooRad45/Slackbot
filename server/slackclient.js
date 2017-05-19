dee9e3e658dc1732dab20c3cbf7ee0d081d8415a
Brendan Rogers <brendan@utexas.edu>
2017-05-18 23:43:56 -0500

    prettified code.

--- server/slackclient.js
+++ server/slackclient.js
@@ -8,26 +8,33 @@ let rtm = null;
 let nlp = null;
 
 function handleOnAuthenticated(rtmStartData) {
-	console.log(`Logged in as ${rtmStartData.self.name} of team ${rtmStartData.team.name}, but not yet connected to a channel`);
+    console.log(`Logged in as ${rtmStartData.self.name} of team ${rtmStartData.team.name}, but not yet connected to a channel`);
 }
 
 function handleOnMessage(message) {
-	nlp.ask(message.text);
-	rtm.sendMessage('this is a test message', message.channel, function messageSent() {
+    nlp.ask(message.text, (err, res) => {
+        if (err) {
+            console.log(err);
+            return;
+        }
 
-	});
+        rtm.sendMessage('Sorry, I did not understand.', message.channel, function messageSent() {
+
+        });
+    });
+
 }
 
 function addAuthenticatedHandler(rtm, handler) {
-	rtm.on(CLIENT_EVENTS.RTM.AUTHENTICATED, handler);
-	return
+    rtm.on(CLIENT_EVENTS.RTM.AUTHENTICATED, handler);
+    return
 }
 module.exports.init = function slackClient(token, logLevel, nlpClient) {
-	rtm = new RtmClient(token, {logLevel: 'logLevel'});
-	nlp = nlpClient;
-	addAuthenticatedHandler(rtm, handleOnAuthenticated);
-	rtm.on(RTM_EVENTS.MESSAGE, handleOnMessage);
-	return rtm;
+    rtm = new RtmClient(token, { logLevel: 'logLevel' });
+    nlp = nlpClient;
+    addAuthenticatedHandler(rtm, handleOnAuthenticated);
+    rtm.on(RTM_EVENTS.MESSAGE, handleOnMessage);
+    return rtm;
 }
 
 module.exports.addAuthenticatedHandler = addAuthenticatedHandler;
--- server/witclient.js
+++ server/witclient.js
@@ -1,12 +1,29 @@
 'use strict';
 
-module.exports = function witClient(token) {
-	const ask = function ask(message) {
-		console.log('ask: ' + message);
-		console.log('token: ' + token);
-	}
+const request = require('superagent');
 
-	return {
-		ask:ask
-	}
+function handleWitResponse(res) {
+    console.log(res);
+}
+
+module.exports = function witClient(token) {
+    const ask = function ask(message, cb) {
+        request.get('https:api.wit.ai/message')
+            .set('Authorization', 'Bearer ' + token)
+            .query({ v: '20170518' })
+            .query({ q: message })
+            .end((err, res)) => {
+                if (err) return cd(err);
+
+                if (res.statusCode != 200) return cb('Expected status 200 but got ' + res.statusCode);
+
+                const witResponse = handleWitResponse(res.body);
+            }
+        console.log('ask: ' + message);
+        console.log('token: ' + token);
+    }
+
+    return {
+        ask: ask
+    }
 }
