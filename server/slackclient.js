abce1d7ed371d5b89b0e32e99b28a9115680d8ef
Brendan Rogers <brendan@utexas.edu>
2017-05-24 23:11:00 -0500

    added timeintent.js and made bot aware of its name

--- server/slackclient.js
+++ server/slackclient.js
@@ -12,31 +12,55 @@ function handleOnAuthenticated(rtmStartData) {
 }
 
 function handleOnMessage(message) {
-    
-    nlp.ask(message.text, (err, res) => {
-        if(err) {
-            console.log(err);
-            return;
-        }
 
-        if(!res.intent) {
-            return rtm.sendMessage("Sorry, I don't know what you are talking about.", message.channel);
+    if (message.text.toLowerCase().includes('audrey')) {
+        nlp.ask(message.text, (err, res) => {
+            if (err) {
+                console.log(err);
+                return;
+            }
 
-        } else if(res.intent[0].value == 'time' && res.location) {
-            return rtm.sendMessage(`I don't yet know the time in ${res.location[0].value}`, message.channel);
-        } else {
-            console.log(res);
-            return rtm.sendMessage("Sorry, I don't know what you are talking about.", message.channel);
-        }
+            try {
+                if (!res.intent || !res.intent[0] || res.intent[0].value) {
+                    throw new Error("Could not extract intent.")
+                }
+
+                const intent = require('./intents/' + res.intent[0].value + 'Intent');
+
+                intent.process(res, function(error, response) {
+                    if (error) {
+                        console.log(error.message);
+                        return;
+                    }
+                    return rtm.sendMessage(response, message.channel);
+                })
+
+            } catch (err) {
+                console.log(err);
+                console.log(res);
+                rtm.sendMessage("Sorry, I don't know what you are talking about.", message.channel);
+            }
+
+            if (!res.intent) {
+                return rtm.sendMessage("Sorry, I don't know what you are talking about.", message.channel);
+
+            } else if (res.intent[0].value == 'time' && res.location) {
+                return rtm.sendMessage(`I don't yet know the time in ${res.location[0].value}`, message.channel);
+            } else {
+                console.log(res);
+                return rtm.sendMessage("Sorry, I don't know what you are talking about.", message.channel);
+            }
 
 
-        rtm.sendMessage('Sorry, I did not understand.', message.channel, function messageSent() {
-            // optionally, you can supply a callback to execute once the message has been sent
+            rtm.sendMessage('Sorry, I did not understand.', message.channel, function messageSent() {
+                // optionally, you can supply a callback to execute once the message has been sent
+            });
         });
-    });
-    
+    }
+
+
 }
-   
+
 
 function addAuthenticatedHandler(rtm, handler) {
     rtm.on(CLIENT_EVENTS.RTM.AUTHENTICATED, handler);
